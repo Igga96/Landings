33b241b4270c642ab9f74659675721f62f4f96a3
Igga96 <91895875+Igga96@users.noreply.github.com>
2022-11-18 06:19:19 +0300

    add sale hidden form

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,7 +1,27 @@
 //buttons
-const btnDark = document.querySelector('.product__button-desktop');
+const btnDark = document.querySelector('.product__button-desktop2');
+const btnOn = document.querySelector('.button__on')
+const btnOff = document.querySelector('.button__off')
 
 btnDark.addEventListener('click', () => {
     document.body.classList.toggle('dark')
-    
+})
+
+
+
+
+const btnSale = document.querySelector('.sale__button')
+btnSale.addEventListener('click', () => {
+    document.body.classList.add('noscroll')
+})
+
+
+
+
+const btnAddCount = document.querySelector('.class="button__minus-count')
+const btnRemoveCount = document.querySelector('.class="button__minus-count')
+const saleFormValue = document.querySelector('sale__count')
+
+btnAddCount.addEventListener('click', function(){
+
 })
