db5d0961b3f865d4087ea43df25b704ded01dcf1
Igga96 <91895875+Igga96@users.noreply.github.com>
2022-11-19 07:11:20 +0300

    fix animation, '.dark', responsive

---
+++ scripts/forms.js
@@ -0,0 +1,68 @@
+////open/close forms/////////необходимо оптимизировать, и функцией сеттаймаут для вызова стейдж2 и закрытием через 3 секунды
+const btnReview = document.querySelector('.review__button')
+const btnSale = document.querySelector('.sale__button')
+const saleForm = document.querySelector('.sale__form-hidden')
+const reviewForm = document.querySelector('.reviews__form-hidden')
+const overlay = document.querySelector('.overlay')
+const btnCards = document.querySelectorAll('.card__button')
+
+btnReview.addEventListener('click', () => {
+    openReviewPopup()
+})
+for(const btnCard of btnCards){
+    btnCard.addEventListener('click', function(){
+openSalePopup()
+    })
+
+}
+
+btnSale.addEventListener('click', () => {
+    openSalePopup()
+})
+
+overlay.addEventListener('click', () => {
+    closePopup()
+})
+
+function openReviewPopup() {
+    myBody.classList.add('noscroll')
+    overlay.style.display = 'block'
+    reviewForm.style.display = 'block'
+}
+
+function openSalePopup() {
+    myBody.classList.add('noscroll')
+    overlay.style.display = 'block'
+    saleForm.style.display = 'block'
+}
+
+function closePopup() {
+    myBody.classList.remove('noscroll')
+    overlay.style.display = 'none'
+    saleForm.style.display = 'none'
+    reviewForm.style.display = 'none'
+}
+
+///////////////////////////////////////// add count lamp in sale form//////////////////////////////////////////////
+let counts = document.querySelectorAll('.sale__count-lamp');
+counts.forEach(countFunc);
+
+function countFunc(count) {
+    let btnPlus = count.querySelector('.button__plus-count');
+    let btnMinus = count.querySelector('.button__minus-count');
+    let saleCount = count.querySelector('.sale__count');
+    let saleValue = parseFloat(saleCount.value, 10);
+
+    btnMinus.addEventListener('click', function () {
+        if (saleValue > 1) {
+            saleValue--;
+            saleCount.value = saleValue;
+        } else {
+            return 1;
+        }
+    });
+    btnPlus.addEventListener('click', function () {
+        saleValue++;
+        saleCount.value = saleValue;
+    });
+}
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,13 +1,15 @@
-//slider
-const btnLeft = document.querySelector('.button__info-left')
-const btnRight = document.querySelector('.button__info-right')
-const slider = document.querySelector('.slider')
-const sliderLine = document.querySelector('.slider__line')
+//info__section - slider
+const scroll = document.querySelector('.info__slider-scroll');//input range
+const btnLeft = document.querySelector('.button__info-left'); //
+const btnRight = document.querySelector('.button__info-right');
+const slider = document.querySelector('.slider');
+const sliderLine = document.querySelector('.slider__line');
 const images = document.querySelectorAll('.slider__image');
 
-
+let scrollValue = scroll.value;
 let count = 0;
 let width;
+
 
 function init() {
     width = slider.offsetWidth;
@@ -24,7 +26,7 @@ window.addEventListener('resize', init);
 
 btnRight.addEventListener('click', function () {
     count++;
-    if (count >= images.length) {
+    if (count >= images.length ) {
         count = 0;
     }
     rollSlider();
@@ -39,10 +41,35 @@ btnLeft.addEventListener('click', function () {
 });
 
 function rollSlider() {
+
     sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+    sliderLine.style.transition = '800ms ease transform';
 
 }
 
+//slider scroll
+scroll.addEventListener("input", () => {
+    let scrollValue = scroll.value
+    sliderLine.style.transition = '800ms ease transform';
+    sliderLine.style.transform = `translateX(${-scrollValue * (width)}px)`;
+});
 
 
+//reviews section
 
+
+const scrollReviews = document.querySelector('.reviews__slider-scroll');
+const reviewSliderLine = document.querySelector('.reviews__slider-line');
+const card = document.querySelector('.review__card');
+const cardWidth = card.clientWidth;
+
+scrollReviews.addEventListener("input", () => {
+    let scrollValue = scrollReviews.value
+
+    if (window.matchMedia("(max-width: 1599px)").matches) {
+        scrollReviews.max = 4;
+    };
+
+    reviewSliderLine.style.transition = '800ms ease transform';
+    reviewSliderLine.style.transform = `translateX(${-scrollValue * (cardWidth)}px)`;
+});
