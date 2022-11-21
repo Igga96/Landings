a873535be396506bd3fd1a9049a70f7b98797d54
Igga96 <91895875+Igga96@users.noreply.github.com>
2022-11-21 05:12:06 +0300

    add new func to forms, fix forms styles

--- scripts/forms.js
+++ scripts/forms.js
@@ -6,46 +6,68 @@ const reviewForm = document.querySelector('.reviews__form-hidden')
 const overlay = document.querySelector('.overlay')
 const btnsCard = document.querySelectorAll('.card__button')
 const btnsFormClose = document.querySelectorAll('.form__close')
+const thankSaleForm = document.querySelector('.form__sale-stage2')
+const thankReviewForm = document.querySelector('.form__reviews-stage2')
 
-
-/*////////////////////////////////////////////////////////////////*
-константа кнопки в форме -событе-
-    func(){блокформы.style.display = 'none';
-    блокновой формы.style.display = 'block'}
-    setTimeout(func, 3000 )
-}
-
-
-
-
-*///////////////////
-btnReview.addEventListener('click', () => {
-    openReviewPopup()
-})
-
-for(const btnFormClose of btnsFormClose){
-    btnFormClose.addEventListener('click', ()=>{
-        closePopup()
-    })
-}
-
-
-
-for(const btnCard of btnsCard){
-    btnCard.addEventListener('click', function(){
-openSalePopup()
+for (const btnCard of btnsCard) {
+    btnCard.addEventListener('click', function () {
+        openSalePopup()
     })
 
 }
-
 
 btnSale.addEventListener('click', () => {
     openSalePopup()
 })
+///////////////////reviews
+btnReview.addEventListener('click', () => {
+    openReviewPopup()
+})
+
+
+/*closePopup*/
+for (const btnFormClose of btnsFormClose) {
+    btnFormClose.addEventListener('click', () => {
+        closePopup()
+    })
+}
 
 overlay.addEventListener('click', () => {
     closePopup()
 })
+
+
+//function say thank you///функции для всплывающего окошка благодароности 1 - после заказа, 2 - после отзыва
+
+/*Далее в настройках формы переходим в "Свойства" -> "PHP скрипты" и в поле $thankYouMessage вставляем:
+$thankYouMessage = ''.$thankYouMessage.'<script type="text/javascript">window.onload = 
+function(){document.getElementById(\'popup_click\').style.display=\'block\';}</script>';
+
+После этого сохраняем всё.
+
+В итоге получаем отправку формы через модуль из всплывающего окна и после отправки 
+всплывает окно благодарности, которое автоматически зарывается через 3000ms .
+Без этого всплывающее окно закрывается после отправки формы.*/
+
+
+function openSalePopupStage2() {
+    myBody.classList.add('noscroll')
+    overlay.style.display = 'block'
+    thankSaleForm.style.display = 'block'
+    setTimeout(() => {
+        closePopup()
+    }, 3000);
+}
+function openReviewPopupStage2() {
+    myBody.classList.add('noscroll')
+    overlay.style.display = 'block'
+    thankReviewForm.style.display = 'block'
+    setTimeout(() => {
+        closePopup()
+    }, 3000);
+}
+/////////////////////////// functions open/close ///////////
+
 
 function openReviewPopup() {
     myBody.classList.add('noscroll')
@@ -64,7 +86,10 @@ function closePopup() {
     overlay.style.display = 'none'
     saleForm.style.display = 'none'
     reviewForm.style.display = 'none'
+    thankSaleForm.style.display = 'none'
+    thankReviewForm.style.display = 'none'
 }
+
 
 ///////////////////////////////////////// add count lamp in sale form//////////////////////////////////////////////
 let counts = document.querySelectorAll('.sale__count-lamp');
