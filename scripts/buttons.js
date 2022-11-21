38de1dfd8541adb7a977b4e38e956a393bf4ff6a
Igga96 <91895875+Igga96@users.noreply.github.com>
2022-11-21 07:07:00 +0300

    fix bg dark

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -4,7 +4,13 @@ const textOn = document.querySelector('.text__on');
 const textOff = document.querySelector('.text__off');
 const textOnHidden = document.querySelector('.text__on-hidden');
 const textOffHidden = document.querySelector('.text__off-hidden');
+const imgHidden = document.querySelector('.hidden__img-active');
+const bg1 = document.querySelector('.container__bg1');
+
+
+
 const myBody = document.body
+
 //add class 'dark' to body
 
 for (const btnDark of btnsDark) {
@@ -19,6 +25,9 @@ function darkBg() {
     textOn.classList.toggle('text__button-active')
     textOffHidden.classList.toggle('text__button-active')
     textOnHidden.classList.toggle('text__button-active')
+    bg1.classList.toggle('bg__active')
+imgHidden.style.display = 'block'
+
 }
 
 
